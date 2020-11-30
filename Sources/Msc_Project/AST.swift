3769d100b6bbe49d3dca89ec3f061a442067bd44
Patrick  Alexandre Sardinha <patrick.sardinha@etu.unige.ch>
2020-11-30 16:04:18 +0100

    AST + FunDecl creation & update main

---
+++ Sources/Msc_Project/AST.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+// AST (base class) is a subclass of Object
+class AST {
+
+}
--- Sources/Msc_Project/main.swift
+++ Sources/Msc_Project/main.swift
@@ -1,20 +1,10 @@
 
-let v1: Vector3 = Vector3(x:1,y:1,z:1)
-let c1: Color = Color(r:1,g:0,b:0)
-print("v1: \(v1)")
+let retourFunc = "void"
+//let nameFunc = Name.ope(Ope.plus)
+let nameFunc = Name.ident("main")
+let bodyFunc = "FragColor = vec4(ourColor, 1.0f);"
 
-readSwiftShader(shaderType: "vertex", name: "sample", ext: "swift")
+let param: Parameter = Parameter(retour: retourFunc, name: nameFunc, body: bodyFunc)
+let function = FunDecl(parameters: param)
 
-
-// ----
-
-let (aPos, loc_aPos): (Vector3, Int) = (v1, 0)
-let (aColor, loc_aColor): (Color, Int) = (c1, 1)
-
-func main() -> (Vector4, Color) {
-     let gl_Position: Vector4 = aPos.cat(v1: aPos, d: 1.0)
-     let ourColor: Color = aColor
-     return (gl_Position, ourColor)
-}
-
-print(main())
+function.build()
