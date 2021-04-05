f1e66f233c5708887afb3324b4598923477e2925
Patrick  Alexandre Sardinha <patrick.sardinha@etu.unige.ch>
2021-04-05 11:56:39 +0200

    fix setter function

--- Sources/PATL/Coord.swift
+++ Sources/PATL/Coord.swift
@@ -18,18 +18,26 @@ public struct Coord {
 
   public var polar: (Double, Angles, Angles) {
     get {
-          var sign: Double
-          switch cart.0 {
-          case let x where x >= 0:
-            sign = 1.0
-          default:
-            sign = -1.0
-          }
-          return (sign*sqrt(pow(cart.0,2) + pow(cart.1,2) + pow(cart.2,2)),
-          Angles(deg: acos(cart.2 / (sign*sqrt(pow(cart.0,2) + pow(cart.1,2) + pow(cart.2,2))))),
-          Angles(deg: 0.0))
+        var sign: Double
+        switch cart.0 {
+        case let x where x >= 0:
+          sign = 1.0
+        default:
+          sign = -1.0
         }
-
+        return (sign*sqrt(pow(cart.0,2) + pow(cart.1,2) + pow(cart.2,2)),
+        Angles(deg: acos(cart.2 / (sign*sqrt(pow(cart.0,2) + pow(cart.1,2) + pow(cart.2,2))))),
+        Angles(deg: 0.0))
+    }
+    set(newVal) {
+        cart = (newVal.0 * sin(newVal.1.deg) * cos(newVal.2.deg),
+                newVal.0 * sin(newVal.1.deg) * sin(newVal.2.deg),
+                newVal.0 * cos(newVal.1.deg))
+    }
   }
-  
+
+
+  public static func cart(_ value: (Double,Double,Double)) -> Coord { Coord(cart: value) }
+
+  public static func pol(_ value: (Double, Angles, Angles)) -> Coord { Coord(polar: value) }
 }
